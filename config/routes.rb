c3ab4508a558c92c13798688c7445a7194970d57
DericChong <deric_chong@hotmail.com>
2014-11-21 20:57:13 +0000

    fixed devise for organizers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SkillCounter::Application.routes.draw do
 
-  devise_for :organizers, controllers: { registrations: "organizer_registrations" }
+  devise_for :organizers, controllers: { registrations: "organizer_registration" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
