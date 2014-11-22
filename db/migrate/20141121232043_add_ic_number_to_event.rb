6d9a0b0266a8b87f7d11c38a939ca09ead9744ba
matheaw <zhiying928@gmail.com>
2014-11-22 00:09:14 +0000

    organizer_sign_up

--- db/migrate/20141121232043_add_ic_number_to_event.rb
+++
@@ -1,6 +0,0 @@
-class AddIcNumberToEvent < ActiveRecord::Migration
-  def change
-    add_column :events, :ic_number, :integer
-    add_index :events, :ic_number
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20141121213627) do
 
-ActiveRecord::Schema.define(version: 20141121201617) do
+  create_table "admins", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contact_numbers", force: true do |t|
     t.datetime "created_at"
@@ -42,54 +47,15 @@ ActiveRecord::Schema.define(version: 20141121201617) do
 
   add_index "events", ["organizer_id"], name: "index_events_on_organizer_id"
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
-ActiveRecord::Schema.define(version: 20141121190209) do
-
-  create_table "admins", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "organizers", force: true do |t|
     t.string   "name"
     t.string   "contact_number"
+    t.string   "email"
     t.string   "address"
     t.integer  "organizerID"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
   end
-
-  add_index "organizers", ["email"], name: "index_organizers_on_email", unique: true
-  add_index "organizers", ["reset_password_token"], name: "index_organizers_on_reset_password_token", unique: true
 
   create_table "user_logins", force: true do |t|
     t.string   "email",                  default: "", null: false
