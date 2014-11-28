b9618efd0e11b1a493b53996dc1c277aa6c2cf00
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-11-28 16:38:37 +0800

    Added Styles, Javascript and Images
    * Add basic static header

--- db/schema.rb
+++ db/schema.rb
@@ -55,31 +55,6 @@ ActiveRecord::Schema.define(version: 20141122015257) do
 
   add_index "events", ["organizer_id"], name: "index_events_on_organizer_id"
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
-  create_table "admins", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "organizers", force: true do |t|
     t.string   "name"
     t.string   "contact_number"
