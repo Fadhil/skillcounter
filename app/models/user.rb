fe0cf9a1f1f12c5dbfae1c459b322ae4b29730d4
matheaw <zhiying928@gmail.com>
2014-11-28 10:11:40 +0000

    fix name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-    validates :name, presence: true, length: { maximum: 50 }
+    validates :name, presence: true, length: { maximum: 50 }, on: :update
     # validates :licence_number, presence: true, uniqueness: true
     # validates :ic_number, presence: true, format: { with: /\A\d{6}\-\d{2}\-\d{4}\z/ }
     # validates :contact_number, presence: true, format: { with: /\A[0][1]\d{1}\-\d{7,8}\z/ }
