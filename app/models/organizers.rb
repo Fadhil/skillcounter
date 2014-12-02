099d4e78bc9ab4b7f9caa6c0d31813034b533aed
DericChong <deric_chong@hotmail.com>
2014-12-02 08:02:40 +0000

    updated organizer file

--- app/models/organizers.rb
+++
@@ -1,6 +0,0 @@
-class Organizers < User
-    
-    validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/, message: 'invalid format' }
-    validates :contact_number, presence: true, format: { with: /\A[0][1]\d{1}\-\d{7,8}\z/ }
-    validates :address, presence: true, length: { maximum: 255 }
-end
