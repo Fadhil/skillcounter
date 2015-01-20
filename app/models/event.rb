c46e115af522ffe78055a1c4e662ff2e8d935c7c
Deric <deric_chong@hotmail.com>
2015-01-20 22:30:31 +0800

    restore foreign key relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
     
-    # belongs_to :organizer
-    # belongs_to :vet
-    belongs_to :user
+    belongs_to :organizer
+    belongs_to :vet
    
-    # belongs_to :creator, :class_name => "User"
+   
+    belongs_to :creator, :class_name => "User"
     has_many :attendances, :foreign_key => "attended_event_id"
     has_many :attendees, :through => :attendances
     scope :upcoming, -> { where("start_date_time >= ?", Date.today).order('start_date_time ASC') }
