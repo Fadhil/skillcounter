879fce7b744e2390a43f948e63a5960715b7ce40
Deric <deric_chong@hotmail.com>
2015-01-21 21:07:02 +0800

    fixed login home path, edit event now no longer remove the file uploads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,26 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     add_flash_types :success, :notice, :alert, :error
     
-    # def after_sign_in_path_for(resource)
-    #   if resource.is_admin?
-    #       redirect_to admin_pending_index_path
-    #   elsif resource.is_organizer?
-    #       redirect_to manage_event_path
-    #   elsif resource.is_vet?
-    #       redirect_to vet_event_path
-    #   elsif resource.is_pending_vet?
-    #       redirect_to transactions_claim_profile_path
-    #   end
-    # end
+    def after_sign_in_path_for(resource)
+      # if resource.is_admin?
+      #     redirect_to admin_pending_index_path
+      # elsif resource.is_organizer?
+      #     redirect_to manage_event_path(current_user)
+      # elsif resource.is_vet?
+      #     redirect_to vet_event_path
+      # elsif resource.is_pending_vet?
+      #     redirect_to transactions_claim_profile_path
+      # end
+    end
     
     rescue_from CanCan::AccessDenied do |exception|  
         flash[:error] = "Access denied! You do not have the access rights to the page. Contact the admin for more information."  
         redirect_to root_url  
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      flash[:error] = "Something went wong. Please contact the admin for more details."
+      redirect_to root_url
     end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
-    if current_user
-      if current_user.is_admin?
-        redirect_to admin_pending_index_path
-      elsif current_user.is_organizer?
-        redirect_to manage_event_path(current_user)
-      elsif current_user.is_vet?
-        redirect_to vet_event_path(current_user)
-      elsif current_user.is_pending_vet?
-        redirect_to transactions_claim_profile_path
-      end
-    else
-      redirect_to new_user_session_path
-    end
+    # if current_user
+    #   if current_user.is_admin?
+    #     redirect_to admin_pending_index_path
+    #   elsif current_user.is_organizer?
+    #     redirect_to manage_event_path(current_user)
+    #   elsif current_user.is_vet?
+    #     redirect_to vet_event_path(current_user)
+    #   elsif current_user.is_pending_vet?
+    #     redirect_to transactions_claim_profile_path
+    #   end
+    # end
   end
   
   def about
